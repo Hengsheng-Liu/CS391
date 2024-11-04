8d8e754bb27cfff370845d59727c2ab744b4c5d0
Hengsheng-Liu <lhs7714@bu.edu>
2024-11-03 20:42:15 -0500

    project template

--- ddl.sql
+++ ddl.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE product_db;
+
 \c product_db
 
 CREATE TABLE products (
@@ -8,5 +9,12 @@ CREATE TABLE products (
     price DECIMAL(10, 2),
     created_at TIMESTAMP DEFAULT NOW()
 );
+CREATE TABLE USERS (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW()
+);  
 COPY products(name, description, price)
 FROM '/docker-entrypoint-initdb.d/product_data.csv' DELIMITER ',' CSV HEADER;
