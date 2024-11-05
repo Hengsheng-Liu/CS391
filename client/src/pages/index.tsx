2a307c80da6dee3fd2bcde4fe34e898d94f2c257
cthuleen <cthuleen@bu.edu>
2024-11-04 21:39:01 -0500

    feat: add empty event cards to content

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-import { Input, Button, Typography, Alert, Table, Pagination, Layout, Menu, theme } from "antd";
+import { Input, Button, Typography, Alert, Table, Pagination, Layout, Menu, theme, Divider, Card, Col, Row } from "antd";
+
+const {Title} = Typography;
 
 const { Header, Content, Footer } = Layout;
 
@@ -12,7 +14,7 @@ export default function Home() {
   return (
     <Layout style={{height:"100vh"}}>
       <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
+        <Title level={2} style={{color:'grey', alignContent: 'center', padding: '0 12px'}}>Spark! Bytes</Title>
         <Menu
           theme="dark"
           mode="horizontal"
@@ -22,8 +24,26 @@ export default function Home() {
         />
       </Header>
       <Content style={{ padding: '0 48px' }}>
+        <Title level={2}>Current Events</Title>
+        <Divider />
         <div>
-          Content
+          <Row gutter={16}>
+          <Col span={8}>
+            <Card title="Event 1" bordered={true}>
+              Event content
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Event 2" bordered={false}>
+              Event content
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card title="Event 3" bordered={false}>
+              Event content
+            </Card>
+          </Col>
+          </Row>
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
