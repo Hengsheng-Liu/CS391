4ce3d76bbdd308d02f33084ecb61a2ae3729523a
cthuleen <cthuleen@bu.edu>
2024-11-04 21:01:57 -0500

    feat: add basic antd layout to index

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,34 @@
 import { useEffect, useState } from "react";
-import { Input, Button, Typography, Alert, Table, Pagination } from "antd";
+import { Input, Button, Typography, Alert, Table, Pagination, Layout, Menu, theme } from "antd";
+
+const { Header, Content, Footer } = Layout;
+
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
 
 export default function Home() {
   return (
-    <div style={{ padding: 20 }}>
-      <Typography.Title level={2}>Home</Typography.Title>
-    </div>
+    <Layout style={{height:"100vh"}}>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <div>
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Spark! Bytes ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
