6ccd915887fd799a754aa6cb1c52f4ed9571e8ec
cthuleen <cthuleen@bu.edu>
2024-11-10 14:18:29 -0500

    feat: add layout from hw5 and update app to implement layout

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
+
 import theme from "@/theme/themeConfig";
+import LayoutComponent from "@/layout/layout";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
-    <Component {...pageProps} />
+    <LayoutComponent>
+      <Component {...pageProps} />
+    </LayoutComponent>
   </ConfigProvider>
 );
 
