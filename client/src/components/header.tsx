64d29227e8de927fbc90499bd627c85afa4357a1
cthuleen <cthuleen@bu.edu>
2024-11-21 17:37:34 -0500

    feat: rework header menu

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -13,10 +13,8 @@ const CustomHeader = () => {
     // label: string
     // href: string (route path) (should not have a trailing-slash, like '/news/'; '/news' is correct.)
     { key: '0', label: 'Home', href: '/' },
-    /* { key: '1', label: 'Post', href: '/post' }, */
-    { key: '1', label: user?.name || "", href: '/profile'},
-    /* { key: '2', label: 'Profile', href: '/profile' }, */
-    /* { key: '3', label: user?.name || "",  href: '', disabled: true }, */
+    { key: '1', label: 'Profile', href: '/profile'},
+    { key: '2', label: user?.name || "",  href: '', disabled: true },
   ];
   // Don't touch this code, use it in your Menu component from Antd
   const router = useRouter();
