1b037fdb8a0037ff8e9682899c4a27177312d882
cthuleen <cthuleen@bu.edu>
2024-12-09 23:06:30 -0500

    chore: add comments to rsvp/index.tsx

--- client/src/pages/api/rsvp/index.tsx
+++ client/src/pages/api/rsvp/index.tsx
@@ -2,18 +2,24 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const Backend = 'http://0.0.0.0:8000';
 
+
+// API handler function to process RSVP-related requests
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
+    // Handle different HTTP methods
     switch (req.method) {
       case 'POST': {
+        // Handle RSVP creation (POST request)
         const {event_id, user_id}= req.body;
         const rsvp = {
             event_id: event_id,
             user_id: user_id,
         }
+
+        // Send POST request to the backend API to create an RSVP
         const response = await fetch(`${Backend}/rsvp/`, {
           method: 'POST',
           headers: {
@@ -27,7 +33,10 @@ export default async function handler(
         }
         return res.status(200).json(data);
         }
+
+      
       case 'DELETE': {
+        // Handle RSVP deletion (DELETE request)
         const {rsvpId,userId} = req.query;
         const response = await fetch(`${Backend}/rsvp/?rsvp_id=${rsvpId}&user_id=${userId}`, {
           method: 'DELETE',
@@ -38,6 +47,8 @@ export default async function handler(
         }
         return res.status(200).json(data);
       }
+
+      // Handle unsupported HTTP methods
       default:
         res.setHeader('Allow', ['GET','POST','DELETE']);
         res.status(405).json({ message: `Method ${req.method} not allowed` });
