b0fc12f2f056e0f75fc93d689a91e45d0d493d60
cthuleen <cthuleen@bu.edu>
2024-12-09 23:16:27 -0500

    chore: add comments to events/[id].tsx

--- client/src/pages/api/events/[id].tsx
+++ client/src/pages/api/events/[id].tsx
@@ -3,14 +3,18 @@ import { useRouter } from 'next/router';
 
 const Backend = 'http://0.0.0.0:8000';
 
+// API handler function to process event-related requests
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+    // Extract the event ID from the query parameters
     const {id} = req.query;
   try {
+    // Handle different HTTP methods
     switch (req.method) {
       case 'GET': {
+         // Handle fetching a specific event (GET request)
         const response = await fetch(`${Backend}/events/event/id/${id}`);
         const data = await response.json();
         if(!response.ok){
@@ -18,7 +22,9 @@ export default async function handler(
         }
         return res.status(200).json(data);
         }
+
       case 'DELETE': {
+        // Handle event deletion (DELETE request)
         const response = await fetch(`${Backend}/events/event/${id}`, {
           method: 'DELETE',
         });
@@ -28,7 +34,9 @@ export default async function handler(
         }
         return res.status(200).json(data);
       }
+
       default:
+        // Handle unsupported HTTP methods
         res.setHeader('Allow', ['GET']);
         res.status(405).json({ message: `Method ${req.method} not allowed` });
     }
